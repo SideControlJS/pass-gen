1fba5d8d1eaacb4954fd9d45f2047c62344ec928
Jesse <jesse.lare@gmail.com>
2023-07-15 03:07:52 -0400

    adding more notes and writing out character sets

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,12 @@
+
+
 // Assignment Code
 // This line assigns the button element with the id attribute 
 // set to  "generate" to the variable generateBtn.  
 // It uses document.querySelectr() method to select the element
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -17,6 +22,8 @@ var generateBtn = document.querySelector("#generate");
 // passwordText.
 // Then it sets the value property of passwordText to the generated password,
 // effectively displaying it in the input field.
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -30,4 +37,13 @@ function writePassword() {
 // The event is the "click" which triggers when the button is clicked.
 // writePassword function is provided as the callback function to be executed when 
 // the "click" event occurs on the button. It will generate and display the password.
+
+
+
 generateBtn.addEventListener("click", writePassword);
+
+
+// Special Characters " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+// lowercase Characters "abcdefghijklmnopqrstuvwxyz"
+// UPPERCASE Letters "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// Password Length min: 8 max: 128
